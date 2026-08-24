1bd69256cc36394d422ae325776c7d3a58122753
ilyaz2000 <ilyazmohammed8099231@gmail.com>
2026-08-24 19:34:15 +0530

    initial project structure

---
+++ OrderService/src/test/java/com/Order/Service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Order.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
